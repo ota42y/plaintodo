ad4eec3c0eae78a76f3317ab57b38ff7f214978f
ota42y <ota42y@gmail.com>
2015-03-25 07:49:56 +0900

    add ls command test

--- ls.go
+++ ls.go
@@ -1,1 +1,10 @@
 package main
+
+type ShowTask struct {
+	Task     *Task
+	SubTasks []*Task
+}
+
+func Ls(tasks []*Task) []*ShowTask {
+	return make([]*ShowTask, 0)
+}
--- ls_test.go
+++ ls_test.go
@@ -1,1 +1,26 @@
 package main
+
+import "testing"
+
+func TestLs(t *testing.T) {
+	filename := "task.txt"
+	tasks := ReadTasks(filename)
+
+	showTasks := Ls(tasks)
+	if len(showTasks) != 2 {
+		t.Errorf("top level shud be 2 but %d", len(showTasks))
+		t.FailNow()
+	}
+
+	subTasks := showTasks[0].SubTasks
+	if len(subTasks) != 2 {
+		t.Errorf("top level shud be 2 but %d", len(subTasks))
+		t.FailNow()
+	}
+
+	subTasks = subTasks[1].SubTasks
+	if len(subTasks) != 3 {
+		t.Errorf("top level shud be 3 but %d", len(subTasks))
+		t.FailNow()
+	}
+}
