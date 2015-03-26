1fbdb95e214200d233129031f9260f67dc337ab9
ota42y <ota42y@gmail.com>
2015-03-26 22:36:43 +0900

    add filter test

--- ls.go
+++ ls.go
@@ -8,3 +8,20 @@ type ShowTask struct {
 func Ls(tasks []*Task) []*ShowTask {
 	return make([]*ShowTask, 0)
 }
+
+func filterRoot(tasks []*Task) []*ShowTask {
+	showTasks := make([]*ShowTask, 0)
+
+	for _, task := range tasks{
+		showTask := filter(task)
+		if showTask != nil{
+			showTasks = append(showTasks, showTask)
+		}
+	}
+
+	return showTasks
+}
+
+func filter(task *Task) *ShowTask {
+	return nil
+}
--- ls_test.go
+++ ls_test.go
@@ -24,3 +24,27 @@ func TestLs(t *testing.T) {
 		t.FailNow()
 	}
 }
+
+/*
+get first task and all sub tasks
+ */
+func TestFilter(t *testing.T){
+	filename := "task.txt"
+	tasks := ReadTasks(filename)
+
+	showTask := filter(tasks[0])
+	if showTask == nil{
+		t.Errorf("filter is nil")
+		t.FailNow()
+	}
+
+	if showTask.Task.Name != tasks[0].Name {
+		t.Errorf("filter isn't valid")
+		t.FailNow()
+	}
+
+	if len(showTask.SubTasks) != 2{
+		t.Errorf("SubTasks num isn't 2")
+		t.FailNow()
+	}
+}
