95372244a01ea4f2984a881118d53cd41d8349d6
ota42y <ota42y@gmail.com>
2015-04-17 07:58:03 +0900

    add output all test

--- plaintodo/output_test.go
+++ plaintodo/output_test.go
@@ -42,3 +42,42 @@ func TestPrint(t *testing.T) {
 	}
 
 }
+
+func TestAllTask(t *testing.T) {
+	tasks := ReadTestTasks()
+	showTasks := Ls(tasks, nil)
+
+	// show all task
+	buf := &bytes.Buffer{}
+	Output(buf, showTasks)
+
+	correctString := `go to SSA :due 2015-02-01
+  create a set list :due 2015-01-31 :important
+    add music to player
+  buy items
+    buy battery
+    buy ultra orange
+    buy king blade
+
+rss
+  my site :url http://ota42y.com :due 2015-02-01 :important :repeat every 1 day
+
+`
+	results := strings.Split(buf.String(), "\n")
+
+	corrects := strings.Split(correctString, "\n")
+
+	if len(corrects) != len(results) {
+		t.Errorf("return %d strings bud %d", len(corrects), len(results))
+		t.FailNow()
+	}
+
+	for index, str := range corrects {
+		if results[index] != str {
+			t.Errorf("return shuld be '%s', but '%s'", str, results[index])
+			t.FailNow()
+		}
+	}
+
+}
+
