755924c5e1cc77dd068734ffc88571d85ac09c43
ota42y <ota42y@gmail.com>
2015-06-07 21:13:41 +0900

    add ls option

--- plaintodo/ls_test.go
+++ plaintodo/ls_test.go
@@ -23,3 +23,39 @@ func TestLs(t *testing.T) {
 		t.FailNow()
 	}
 }
+
+func TestGetQuery(t *testing.T) {
+	tasks := ReadTestTasks()
+
+	query := GetQuery(" :level 2")
+	base := query.(*QueryBase)
+
+	if len(base.and) == 0 {
+		t.Errorf("MaxLevelQuery dosen't created")
+		t.FailNow()
+	}
+
+	showTasks := Ls(tasks, query)
+
+	if len(showTasks) == 0 {
+		t.Errorf("return no tasks")
+		t.FailNow()
+	}
+
+	for _, task := range showTasks {
+		if 2 < task.Task.Level {
+			t.Errorf("Task.Level shuld be less than 2 but %v", task.Task)
+			t.FailNow()
+		}
+		for _, subTask := range task.SubTasks {
+			if 2 < subTask.Task.Level {
+				t.Errorf("Task.Level shuld be less than 2 but %v", task.SubTasks)
+				t.FailNow()
+			}
+			if len(subTask.SubTasks) != 0 {
+				t.Errorf("SubTasks shuld be 0 but %d", len(subTask.SubTasks))
+				t.FailNow()
+			}
+		}
+	}
+}
